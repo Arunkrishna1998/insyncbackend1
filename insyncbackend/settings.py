8eb12e146497bc99aea4358df87da8929f844b1b
Arunkrishna1998 <arunkvb98@gmail.com>
2024-01-11 21:16:02 +0530

    db changed

--- insyncbackend/settings.py
+++ insyncbackend/settings.py
@@ -123,12 +123,25 @@ ASGI_APPLICATION = 'insyncbackend.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': ' insyncdb',
+        'USER': ' insyncuser',
+        'PASSWORD': 'insync@123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
